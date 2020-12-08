bd9a90cd75bb2da02ec5b84ae5fa9ecac7f088fd
Hugo <hugoribeiro918@gmail.com>
2020-12-08 20:35:07 +0000

    Server_class

---
+++ Server/src/server/Server.java
@@ -0,0 +1,42 @@
+package server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Hugo
+ */
+public class Server{
+    int port
+    InetAddress addr;
+    int nClientes;
+    /**
+     * @param args the command line arguments
+     */
+    public static void Server(int port, InetAddress addr){
+        this.port = port;
+	this.addr = addr;
+	nClientes = 0;
+    }
+
+    public InetAddress getAddress(){
+	return addr;
+    }
+
+    public int getPort(){
+	return port;
+    }
+
+    public void incrementa(){
+	nClientes++;
+    }
+
+    public int percentagem(int lotacao){
+	return nClientes*100/lotacao;
+    }
+}
